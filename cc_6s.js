c19317293e2942e98bd5b06177f1f34fa067ce3b
ssj2-z <ssj2@usf.edu>
2025-02-04 15:10:02 -0500

    Task 2 - Sales Tax Computation.

--- cc_6s.js
+++ cc_6s.js
@@ -3,3 +3,9 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
     const profit = (sellingPrice - costPrice) * unitsSold;
     console.log(`Total Profit: $${profit}`); return profit;
 }
+
+// Task 2 Function Expression
+const calculateSalesTax = function(amount, taxRate) {
+    const salesTax = amount * taxRate;
+    console.log(`Sales Tax: $${salesTax.toFixed(2)}`); return salesTax;
+};
