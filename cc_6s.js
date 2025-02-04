795b8bb5773e320de23270c8128dc77ca227e7e4
ssj2-z <ssj2@usf.edu>
2025-02-04 15:21:45 -0500

    Refresh and updated

--- cc_6s.js
+++ cc_6s.js
@@ -2,6 +2,8 @@
 function calculateProfit(costPrice, sellingPrice, unitsSold) {
     const profit = (sellingPrice - costPrice) * unitsSold;
     console.log(`Total Profit: $${profit}`); return profit;
+    calculateProfit(10, 25, 15); // Expected output: "Total Profit: $150"
+    calculateProfit(20, 35, 55); // Expected output: "Total Profit: $275"
 }
 
 // Task 2 Function Expression
@@ -9,3 +11,5 @@ const calculateSalesTax = function(amount, taxRate) {
     const salesTax = amount * taxRate;
     console.log(`Sales Tax: $${salesTax.toFixed(2)}`); return salesTax;
 };
+calculateSalesTax(100, .07); // Expected output: "Sales Tax: $7.00"
+calculateSalesTax(500, .1); // Expected output: "Sales Tax: $50.00"
