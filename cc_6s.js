c19bca2653c82cbe3bb655ea88ae46529605c6f3
ssj2-z <ssj2@usf.edu>
2025-02-04 15:05:04 -0500

    Task 1 - Business Profit Calculation.

---
+++ cc_6s.js
@@ -0,0 +1,5 @@
+// Task 1 Function Declaration
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    const profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`); return profit;
+}
